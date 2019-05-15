3f54a40f2521baa3bbd3e8a76a9f5831828dc1c1
albrdev <albrdev@gmail.com>
2019-05-15 15:12:10 +0200

    clenaup

--- networking/packet_receiver.py
+++ networking/packet_receiver.py
@@ -66,7 +66,6 @@ class PacketReceiver(object):
             n = header.size if header is not None else PacketHeader.SIZE
 
             avail = self._available_bytes(s.fileno())
-            print("debug(avail={0}, n={1})".format(avail, n))
             if avail < n:
                 if avail <= 0:
                     self.on_client_disconnected(s.getpeername())
